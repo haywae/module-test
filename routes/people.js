0c3d57b8486f21b14e6b163722396085d47e4fee
haywae <haywae35@gmail.com>
2023-11-25 12:24:45 +0200

    updated full course

---
+++ routes/people.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+const {
+    getPeople, 
+    createPerson,
+    createPersonPostman,
+    updatePerson,
+    deletePerson
+} = require('../controller/people')
+
+
+
+router.get('/', getPeople)
+
+router.post('/', createPerson)
+
+router.post('/postman', createPersonPostman)
+
+router.put('/postman/:id', updatePerson)
+
+router.delete('/:id', deletePerson)
+
+module.exports = router
